f704f3f731df5fef21c9b8a5a0be3e8137d308ff
janabaevbaxadir7-netizen <janabaevbaxadir7@gmail.com>
2026-07-01 11:01:48 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,6 @@
-import os
+BOT_TOKEN = "8902500884:AAHb8NryHR-3M3xoviXeN_YdNA61tRkz0Ik"
 
-BOT_TOKEN = os.getenv("BOT_TOKEN", "BOT_TOKENINGIZNI_BU_YERGA_YOZING")
-ADMIN_IDS = [
-    int(x) for x in os.getenv("ADMIN_IDS", "0").split(",") if x.strip().isdigit()
-]
-DB_PATH = os.getenv("DB_PATH", "sakura.db")
+ADMIN_IDS = [8094557015]
+
+DB_PATH = "sakura.db"
 BOT_NAME = "Yuki-chan"
